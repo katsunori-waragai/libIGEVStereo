24e765de2fa056ba853f900f93828a5fa65d4623
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-11 14:19:35 +0900

    under develop

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-docker build -t igev:100 .
+docker build -t libigev:100 .
   
--- docker_run.sh
+++ docker_run.sh
@@ -2,8 +2,8 @@
 xhost +
 export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${GIT_ROOT}/IGEV-docker/test_data:/root/IGEV/test_data \
+	-v ${GIT_ROOT}/libIGEVStreo/test_data:/root/libIGEVStreo/test_data \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
-	-v /tmp/.X11-unix/:/tmp/.X11-unix igev:100
+	-v /tmp/.X11-unix/:/tmp/.X11-unix libigev:100
  
