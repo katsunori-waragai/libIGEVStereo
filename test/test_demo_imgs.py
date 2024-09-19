27c184af06e03f3669d87b6c68bd83d338de9e3c
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-19 12:05:24 +0900

    make test passed

--- test/test_demo_imgs.py
+++ test/test_demo_imgs.py
@@ -14,7 +14,7 @@ from tqdm import tqdm
 from stereoigev.lib_disparity import DisparityCalculator
 
 
-def demo(args):
+def demo(args: argparse.Namespace):
     disparity_calculator = DisparityCalculator(args=args)
     output_directory = Path(args.output_directory)
     output_directory.mkdir(exist_ok=True)
@@ -48,14 +48,14 @@ def test_all():
         corr_levels=2,
         corr_radius=4,
         hidden_dims=[128, 128, 128],
-        left_imgs="test-imgs/PlaytableP/im0.png",
+        left_imgs="test-imgs/left/left*.png",
         max_disp=192,
         mixed_precision=False,
         n_downsample=2,
         n_gru_layers=3,
         output_directory="./test-output/",
         restore_ckpt="../stereoigev/models/sceneflow.pth",
-        right_imgs="test-imgs/PlaytableP/im1.png",
+        right_imgs="test-imgs/right/right*.png",
         save_numpy=True,
         shared_backbone=False,
         slow_fast_gru=False,
