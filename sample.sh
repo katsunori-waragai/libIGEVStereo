fa77aba6f1671fb47f51b54426fbb7751441a410
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-19 11:46:07 +0900

    chaned test-image foloder

--- sample.sh
+++ sample.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-python3 demo_imgs.py --restore_ckpt ./stereoigev/models/sceneflow.pth -l "test/test-imgs/left/left*.png" -r "test/test-imgs/Pright/right*.png"
+python3 demo_imgs.py --restore_ckpt ./stereoigev/models/sceneflow.pth -l "test/test-imgs/left/left*.png" -r "test/test-imgs/right/right*.png"
  
