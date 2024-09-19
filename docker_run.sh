b3ca511ba58ff11bf110f07e033198ba4d089b03
Katsunori Waragai <149886409+katsunori-waragai@users.noreply.github.com>
2024-09-19 13:37:21 +0900

    Feature/demo in module (#30)
    why
    doubled function demo() exists.
    what
    removed doubled one.

--- docker_run.sh
+++ docker_run.sh
@@ -2,7 +2,6 @@
 xhost +
 export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
-	-v ${GIT_ROOT}/libIGEVStereo/test_data:/root/libIGEVStereo/test_data \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix libigev:100
