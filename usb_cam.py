1148d415dd77f74fca5fa0194df4cdc5a7f34b30
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-21 17:12:37 +0900

    show both

--- usb_cam.py
+++ usb_cam.py
@@ -73,11 +73,11 @@ if __name__ == "__main__":
             if calc_disparity:
                 disparity = disparity_calculator.predict(left.copy(), right.copy())
                 disp = np.round(disparity * 256).astype(np.uint16)
-                if normal:
-                    colored = converter.convert(disp)
-                else:
-                    colored = cv2.applyColorMap(cv2.convertScaleAbs(disp, alpha=0.01), cv2.COLORMAP_JET)
+                colored = cv2.applyColorMap(cv2.convertScaleAbs(disp, alpha=0.01), cv2.COLORMAP_JET)
                 cv2.imshow("IGEV", colored)
+                if normal:
+                    normal_bgr = converter.convert(disp)
+                    cv2.imshow("normal", normal_bgr)
             key = cv2.waitKey(100)
             if key == ord("q"):
                 exit()
