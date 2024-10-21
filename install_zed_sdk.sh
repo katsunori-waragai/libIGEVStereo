964552e295541fe570e294adb9f4d2e9d93296a3
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-21 19:10:01 +0900

    add install_zed_sdk.sh

---
+++ install_zed_sdk.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update
+apt install sudo
+apt install -y zip
+apt install zstd
+export ZED_SDK_INSTALLER=ZED_SDK_Tegra_L4T35.3_v4.1.0.zstd.run
+wget --quiet -O ${ZED_SDK_INSTALLER} https://download.stereolabs.com/zedsdk/4.1/l4t35.2/jetsons
+chmod +x ${ZED_SDK_INSTALLER} && ./${ZED_SDK_INSTALLER} -- silent
