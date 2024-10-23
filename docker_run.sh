629c1c6525b2fcca865db7ee7e309d905268d8a9
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-10-23 15:30:31 +0900

    mount model_zoo

--- docker_run.sh
+++ docker_run.sh
@@ -6,5 +6,6 @@ docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
 	-v ${GIT_ROOT}/libIGEVStereo/mounted_data:/root/libIGEVStereo/mounted_data \
+	-v ${GIT_ROOT}/libIGEVStereo/model_zoo:/root/libIGEVStereo/model_zoo \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix libigev:100
  
